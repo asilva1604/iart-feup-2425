cf5d713be0159f065f2c9489a1281e415c7045f3
Alexandre Silva <asilva1604@outlook.com>
2025-03-13 09:18:32 +0000

    correct Hill Climbing algo

--- src/classes.py
+++ src/classes.py
@@ -116,13 +116,12 @@ class HillClimbing:
         self.best_plan = seating_plan
         self.current_score = seating_plan.score()
         self.best_score = self.current_score
-        self.maxiterations = max_iterations
+        self.max_iterations = max_iterations
 
     def run(self):
-        for _ in range(self.maxiterations):
+        for _ in range(self.max_iterations):
             new_plan = self._generate_neighbor()
             new_score = new_plan.score()
-
 
             if new_score > self.current_score:
                 self.current_plan = new_plan
@@ -131,14 +130,16 @@ class HillClimbing:
                 if new_score > self.best_score:
                     self.best_plan = new_plan
                     self.best_score = new_score
-            else:
-                break
 
         return self.best_plan, self.best_score
 
     def _generate_neighbor(self):
-        """Generates a neighboring solution by swapping guests between tables."""    
+        """Generates a neighboring solution by swapping guests between tables."""
         new_plan = SeatingPlan(self.current_plan.guest_list, len(self.current_plan.tables), self.current_plan.tables[0].capacity)
+        new_plan.tables = [Table(table.capacity) for table in self.current_plan.tables]
+        for i, table in enumerate(self.current_plan.tables):
+            new_plan.tables[i].guests = table.guests[:]
+
         table1, table2 = random.sample(range(len(new_plan.tables)), 2)
         new_plan.swap_guests(table1, table2)
         return new_plan
