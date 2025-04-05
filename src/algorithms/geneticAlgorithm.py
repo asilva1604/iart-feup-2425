7dbada615730c4e1a7e8ea0b08b1e438bd6a9a56
Bruno Fortes <brunofortes2002@gmail.com>
2025-04-05 19:14:54 +0100

    Fixing GA

--- src/algorithms/geneticAlgorithm.py
+++ src/algorithms/geneticAlgorithm.py
@@ -1,68 +1,85 @@
 import random
 from seating_plan import SeatingPlan
 
-
 class GeneticAlgorithm:
-    def __init__(self, guests, num_tables, table_capacity, population_size=50, generations=100, mutation_rate=0.1):
+    def __init__(self, guests, num_tables, table_capacity, population_size=30, generations=50, mutation_rate=0.1, elitism_rate=0.1):
         self.guests = guests
         self.num_tables = num_tables
         self.table_capacity = table_capacity
         self.population_size = population_size
         self.generations = generations
         self.mutation_rate = mutation_rate
+        self.elitism_rate = elitism_rate  # Percentage of top individuals to retain
+        self.fitness_cache = {}  # Cache for fitness evaluations
 
     def initialize_population(self):
-        self.population = [SeatingPlan(self.guests,self.num_tables,self.table_capacity) for _ in range(self.population_size)]
-        return self.population
+        """Initialize the population with random seating plans."""
+        return [SeatingPlan(self.guests, self.num_tables, self.table_capacity) for _ in range(self.population_size)]
 
     def fitness(self, seating_plan):
-        return seating_plan.score()
-    
-    def selection(self, population):
-        fitness_score = [self.fitness(plan) for plan in population]
-        total_fitness = sum(fitness_score)
-        if total_fitness == 0:
-            return random.sample(population, 2)
-        prob = [score / total_fitness for score in fitness_score]
-        return random.choices(population, weights=prob, k=2)
-    
+        """Calculate the fitness (score) of a seating plan, using caching."""
+        plan_key = tuple(tuple(table.guests) for table in seating_plan.tables)
+        if plan_key not in self.fitness_cache:
+            self.fitness_cache[plan_key] = seating_plan.score()
+        return self.fitness_cache[plan_key]
+
+    def selection(self, population, fitness_scores):
+        """Select two parents using tournament selection."""
+        tournament_size = 3
+        selected = random.choices(population, k=tournament_size)
+        selected_scores = [fitness_scores[population.index(ind)] for ind in selected]
+        return selected[selected_scores.index(max(selected_scores))]
+
     def crossover(self, parent1, parent2):
+        """Perform crossover between two parents to produce a child."""
         child = parent1.copy()
         for i in range(len(child.tables)):
             if random.random() < 0.5:
                 child.tables[i].guests = parent2.tables[i].guests[:]
         return child
-    
+
     def mutation(self, seating_plan):
+        """Mutate a seating plan by swapping guests between tables."""
         if random.random() < self.mutation_rate:
             table1, table2 = random.sample(range(self.num_tables), 2)
-            if seating_plan.tables[table1].guests:
-                guest = random.choice(seating_plan.tables[table1].guests)
-                seating_plan.move_guest(guest, table1, table2)
+            if seating_plan.tables[table1].guests and seating_plan.tables[table2].guests:
+                guest1 = random.choice(seating_plan.tables[table1].guests)
+                guest2 = random.choice(seating_plan.tables[table2].guests)
+                seating_plan.tables[table1].remove_guest(guest1)
+                seating_plan.tables[table2].remove_guest(guest2)
+                seating_plan.tables[table1].add_guest(guest2)
+                seating_plan.tables[table2].add_guest(guest1)
 
     def run(self):
+        """Run the Genetic Algorithm to optimize the seating plan."""
         population = self.initialize_population()
         best_plan = None
         best_score = float('-inf')
 
         for gen in range(self.generations):
-            population = sorted(population, key=self.fitness, reverse=True)
-            if self.fitness(population[0]) > best_score:
-                best_plan = population[0]
-                best_score = self.fitness(best_plan)
-            
-            next_generation = []
+            # Evaluate fitness for the population
+            fitness_scores = [self.fitness(plan) for plan in population]
+
+            # Update the best solution
+            max_fitness_idx = fitness_scores.index(max(fitness_scores))
+            if fitness_scores[max_fitness_idx] > best_score:
+                best_plan = population[max_fitness_idx]
+                best_score = fitness_scores[max_fitness_idx]
+
+            # Elitism: Retain the top individuals
+            num_elites = max(1, int(self.elitism_rate * self.population_size))
+            elites = sorted(zip(population, fitness_scores), key=lambda x: x[1], reverse=True)[:num_elites]
+            next_generation = [elite[0] for elite in elites]
+
+            # Generate the rest of the next generation
             while len(next_generation) < self.population_size:
-                parent1, parent2 = self.selection(population)
+                parent1 = self.selection(population, fitness_scores)
+                parent2 = self.selection(population, fitness_scores)
                 child = self.crossover(parent1, parent2)
                 self.mutation(child)
                 next_generation.append(child)
 
             population = next_generation
 
-            if best_plan is None:
-                best_plan = population[0]
-
         return best_plan, best_score
 
-        
